1cae2bae26a05791ec50379f461c9eada7b3c8a9
Daer0n <danilatarbaya57@gmail.com>
2024-02-25 01:00:06 +0300

    feat(routers.py): add save and search

--- EYAZIS/laba1/backend/src/functions.py
+++ EYAZIS/laba1/backend/src/functions.py
@@ -4,65 +4,89 @@ import pymorphy2
 from schemas import Word
 
 def get_lexemes(text):
-    # разбиваем текст на слова
     words = nltk.word_tokenize(text.lower(), language='russian')
-    # создаем объект MorphAnalyzer для морфологического анализа слов
     morph = pymorphy2.MorphAnalyzer()
     lexemes = []
     for word in words:
-        # пропускаем знаки препинания
         if not word.isalpha():
             continue
-        # получаем морфологические характеристики слова
         parse = morph.parse(word)[0]
-        # создаем запись для каждой лексемы
         lexeme = {
-            'base': parse.normal_form,  # основа слова
-            'pos': parse.tag.POS,  # часть речи
-            'gender': parse.tag.gender,  # род
-            'number': parse.tag.number,  # число
-            'case': parse.tag.case,  # падеж
-            'tense': parse.tag.tense,  # время (для глаголов)
-            'aspect': parse.tag.aspect  # вид (для глаголов)
+            'base': parse.normal_form,  
+            'pos': parse.tag.POS, 
+            'gender': parse.tag.gender, 
+            'number': parse.tag.number,  
+            'case': parse.tag.case, 
+            'tense': parse.tag.tense,  
+            'aspect': parse.tag.aspect  
         }
-        # добавляем лексему в список
         lexemes.append(lexeme)
-    # сортируем список лексем по алфавиту
     lexemes.sort(key=lambda x: x['base'])
     return lexemes
 
 def generate_word_form(word: Word):
-    """
-    Функция генерирует словоформу по заданным параметрам
-    word: базовая форма слова
-    gender: род: "муж", "жен", "сред"
-    case: падеж: "им", "род", "дат", "вин", "твор", "пр", "мест"
-    number: число: "ед", "мн"
-    return: словоформа в соответствии с заданными параметрами
-    """
     morph = pymorphy2.MorphAnalyzer()
     parse = morph.parse(word.base)[0]
-    tags = {"муж": "masc", "жен": "femn", "сред": "neut", "им": "nomn", "род": "gent",
-            "дат": "datv", "вин": "accs", "твор": "ablt", "пр": "loct","ед": "sing", "мн": "plur"}
+    tags = {
+        "муж": "masc",
+        "жен": "femn", 
+        "сред": "neut", 
+        "им": "nomn", 
+        "род": "gent",
+        "дат": "datv",
+        "вин": "accs",
+        "твор": "ablt", 
+        "пр": "loct",
+        "ед": "sing", 
+        "мн": "plur"
+    }
     gram_info = {tags[v] for v in {word.gender, word.case, word.number}}
     return parse.inflect(gram_info).word
 
 def get_lexems_to_text(lexems):
-    txt = ""
-    tags = {"NOUN":"сущ.", "ADJF":"прилаг.", "ADJS":"прилаг.", "COMP":"компаратив", "VERB":"глагол", "INFN":"глагол","PRTF":"причастие",
-            "PRTS":"причастие", "GRND":"дееприч.", "NUMR":"числит.", "ADVB": "наречие", "NRPO":"местоим.-сущ.","PRED":"предикатив",
-            "PREP":"предлог","CONJ":"союз","PRCL":"частица","INTJ":"междометие","nomn":"Им.п.", "gent":"Род.п.","datv":"Дат.п.",
-            "accs":"Вин.п.","ablt":"Твор.п.","loct":"Пред.п.","sing":"ед.ч.","plur":"мн.ч","masc":"муж.р.","femn":"жен.р.","neut":"ср.р"}
+    result = []
+    tags = {
+        "NOUN": "существительное",
+        "ADJF": "прилагательное",
+        "ADJS": "прилагательное",
+        "COMP": "компаратив",
+        "VERB": "глагол",
+        "INFN": "глагол",
+        "PRTF": "причастие",
+        "PRTS": "причастие",
+        "GRND": "деепричастие",
+        "NUMR": "числительное",
+        "ADVB": "наречие",
+        "NRPO": "местоимение-существительное",
+        "PRED": "предикатив",
+        "PREP": "предлог",
+        "CONJ": "союз",
+        "PRCL": "частица",
+        "INTJ": "междометие",
+        "nomn": "им.п.",
+        "gent": "род.п.",
+        "datv": "дат.п.",
+        "accs": "вин.п.",
+        "ablt": "твор.п.",
+        "loct": "пред.п.",
+        "sing": "ед.ч.",
+        "plur": "мн.ч",
+        "masc": "муж.р.",
+        "femn": "жен.р.",
+        "neut": "ср.р"
+    }
+    
     for item in lexems:
-        txt += "Основа: "
-        txt += item['base'] + ", ч.речи: "
-        txt += tags[item['pos']]
-        if item['gender'] != None:
-            txt +=", род: " + tags[item['gender']]
-        if item['number'] !=None:
-            txt +=', число: ' + tags[item['number']]
-        if item['case'] != None:
-            txt +=', падеж: ' + tags[item['case']]
-
-        txt +='\n'
-    return txt
+        lexem = {
+            'Основа': item['base'],
+            'ч.речи': tags[item['pos']]
+        }
+        if item['gender'] is not None:
+            lexem['род'] = tags[item['gender']]   
+        if item['number'] is not None:
+            lexem['число'] = tags[item['number']]
+        if item['case'] is not None:
+            lexem['падеж'] = tags[item['case']]
+        result.append(lexem)
+    
+    return result
--- EYAZIS/laba1/backend/src/routers.py
+++ EYAZIS/laba1/backend/src/routers.py
@@ -1,4 +1,5 @@
 import io
+import json
 from docx import Document
 from fastapi import APIRouter, UploadFile
 from fastapi import HTTPException, status
@@ -28,3 +29,44 @@ async def sentence_parsing(file: UploadFile):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File not found")
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unprocessable entity")
+    
+
+@router.post('/save')
+async def sentence_parsing(file: UploadFile):
+    try:
+        file_content = await file.read()
+        file_object = io.BytesIO(file_content)
+        doc = Document(file_object)
+        sentence = '\n'.join([paragraph.text for paragraph in doc.paragraphs])
+        lexemes = get_lexems_to_text(get_lexemes(sentence))
+        
+        with open('data/data.json', 'a') as json_file:
+            for lexeme in lexemes:
+                json_data = json.dumps(lexeme, ensure_ascii=False)
+                json_file.write(json_data + '\n')
+        
+        return {'ok' : True}
+    except FileNotFoundError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File not found")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unprocessable entity")
+    
+@router.post('/base/search')
+async def base_search(base: str):
+    with open('data/data.json') as json_file:
+        data = []
+        for line in json_file:
+            lexeme = json.loads(line)
+            if lexeme['Основа'] == base:
+                data.append(lexeme)
+    return data
+
+@router.post('/part/of/speech/search')
+async def part_of_speech_search(base: str):
+    with open('data/data.json') as json_file:
+        data = []
+        for line in json_file:
+            lexeme = json.loads(line)
+            if lexeme['ч.речи'] == base:
+                data.append(lexeme)
+    return data
